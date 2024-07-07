2bfe1f2e5e23a9f5444487aceaa2732b0aa8116e
silvia80569 <157542289+silvia80569@users.noreply.github.com>
2024-07-07 16:59:06 +0000

    Deploying to gh-pages from @ silvia80569/goit-js-hw-09@24dfac5376d33d11c0ffbe171f75896b7c5061ae 🚀

---
+++ 01-color-switcher.11fe6c5c.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]");let a=null;t.addEventListener("click",(()=>{a||(t.disabled=!0,a=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,"0")}`}),1e3))})),e.addEventListener("click",(()=>{a&&(clearInterval(a),a=null,t.disabled=!1)}));
+//# sourceMappingURL=01-color-switcher.11fe6c5c.js.map
