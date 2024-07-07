24dfac5376d33d11c0ffbe171f75896b7c5061ae
silvia80569 <157542289+silvia80569@users.noreply.github.com>
2024-07-07 18:58:41 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0')}`;
+}
+
+const startButton = document.querySelector('[data-start]');
+const stopButton = document.querySelector('[data-stop]');
+let colorInterval = null;
+
+startButton.addEventListener('click', () => {
+  if (colorInterval) return;  
+  startButton.disabled = true;
+  colorInterval = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+});
+
+stopButton.addEventListener('click', () => {
+  if (!colorInterval) return;  
+  clearInterval(colorInterval);
+  colorInterval = null;
+  startButton.disabled = false;
+});
 
